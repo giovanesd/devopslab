9bb09a3e1ed0702f2fcd8c8036d6e5be54782711
giovanesd <65796255+giovanesd@users.noreply.github.com>
2022-03-13 13:32:53 -0300

    Test Fail Pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Grupo 16 - Giovane de Deus"
+    return "Hello World - Grupo 16 - Giovane de Deus - FIAP"
 
 if __name__ == '__main__':
     app.run()
