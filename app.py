b715654e35197114cf4423e6bbb03c6f7bf1347e
Wiliam Buzatto <wiliambuzatto@gmail.com>
2022-03-30 14:58:07 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ csrf = CSRFProtect(app)
 
 @app.route("/")
 def pagina_inicial():
-    return "Hello World - Grupo 16 - Giovane de Deus - FIAP"
+    return "Hello World - Grupo 16 - FIAP"
 
 if __name__ == '__main__':
     port = os.getenv('PORT')
